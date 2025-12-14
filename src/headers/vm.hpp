ca3c1504ca80840bb67ecb2d0336ad36eb7fb9e2
mustafa-elrasheid <mustafaelrasheid@gmail.com>
2025-12-14 16:58:47 +0300

    whitespace added & renaming registers

--- src/headers/vm.hpp
+++ src/headers/vm.hpp
@@ -9,17 +9,17 @@
 #include "bytecode.hpp"
 
 union address_as_int{
-    uint64_t value;
-    uint64_t* address;
+	uint64_t value;
+	uint64_t* address;
 };
 
 struct VirtualMachine{
-    int _stack_size;
-    int64_t* Registers;
-    int64_t* Stack;
-    VirtualMachine(int64_t entrypoint, char* data_Section, int64_t stack_size, int64_t registers_num);
-    inline int64_t parameter_value(Parameter* param);
-    inline int64_t binary_operator(InstructionType type, int64_t value1, int64_t value2);
-    void Run(InstructionList* instructions,bool log);
-    ~VirtualMachine();
+	int _stack_size;
+	int64_t* Registers;
+	int64_t* Stack;
+	VirtualMachine(int64_t entrypoint, char* data_Section, int64_t stack_size, int64_t registers_num);
+	inline int64_t parameter_value(Parameter* param);
+	inline int64_t binary_operator(InstructionType type, int64_t value1, int64_t value2);
+	void Run(InstructionList* instructions, bool log);
+	~VirtualMachine();
 };
--- src/vm.cpp
+++ src/vm.cpp
@@ -15,36 +15,36 @@ inline int64_t VirtualMachine::parameter_value(Parameter* param){
 }
 
 inline int64_t VirtualMachine::binary_operator(InstructionType type,int64_t value1,int64_t value2){
-    switch(type){
-        case ADD : return  value1 +  value2;
-        case SUB : return  value1 -  value2;
-        case DIV : return  value1 /  value2;
-        case MUL : return  value1 *  value2;
-        case REM : return  value1 %  value2;
-        case AND : return  value1 &  value2;
-        case OR  : return  value1 |  value2;
-        case XOR : return  value1 ^  value2;
-        case NOT : return !value1          ;
-        case CMP : return  value1 == value2;
-        case CMPB: return  value1 >  value2;
-    }
-    return 0;
+	switch(type){
+		case ADD : return  value1 +  value2;
+		case SUB : return  value1 -  value2;
+		case DIV : return  value1 /  value2;
+		case MUL : return  value1 *  value2;
+		case REM : return  value1 %  value2;
+		case AND : return  value1 &  value2;
+		case OR  : return  value1 |  value2;
+		case XOR : return  value1 ^  value2;
+		case NOT : return !value1          ;
+		case CMP : return  value1 == value2;
+		case CMPB: return  value1 >  value2;
+	}
+	return 0;
 }
 
 VirtualMachine::VirtualMachine(int64_t entrypoint,char* data_Section,int64_t stack_size,int64_t registers_num){
 	Stack = (int64_t*)calloc(stack_size*sizeof(int64_t),1);
 	_stack_size = stack_size;
 	Registers = (int64_t*)calloc(registers_num*sizeof(int64_t),1);
-	Registers[RegisterType::SP]   = (int64_t)&Stack[stack_size];
-	Registers[RegisterType::SP]   = (int64_t)&Stack[stack_size];
+	Registers[RegisterType::RSP]   = (int64_t)&Stack[stack_size];
+	Registers[RegisterType::RSP]   = (int64_t)&Stack[stack_size];
 	Registers[RegisterType::data] = (int64_t)data_Section;
-	Registers[RegisterType::IP]   = entrypoint;
+	Registers[RegisterType::RIP]   = entrypoint;
 	Registers[RegisterType::Null] = 0;
 }
 
 void VirtualMachine::Run(InstructionList* instructions,bool log){
-	for(;true;Registers[RegisterType::IP]++){
-		Instruction* instruction = instructions->instructions[Registers[RegisterType::IP]];
+	for(;true;Registers[RegisterType::RIP]++){
+		Instruction* instruction = instructions->instructions[Registers[RegisterType::RIP]];
 		int64_t* args;
 		int args_count;
 		if(log){
@@ -52,41 +52,42 @@ void VirtualMachine::Run(InstructionList* instructions,bool log){
 			printf("[");
 			for(int x = 0;x < 20;x++){
 				printf(" ");
-				if(Registers[RegisterType::SP] == (int64_t)&Stack[_stack_size - x])
+				if(Registers[RegisterType::RSP] == (int64_t)&Stack[_stack_size - x])
 					printf("S");
-				else if(Registers[RegisterType::BP] == (int64_t)&Stack[_stack_size - x])
+				else if(Registers[RegisterType::RBP] == (int64_t)&Stack[_stack_size - x])
 					printf("B");
 				else
 					printf(" ");
 				printf("%#x ",Stack[_stack_size - x]);
 			}
 			printf("] ");
-			printf("AX:%#x ",Registers[RegisterType::AX]);
-			printf("IP:%#x ",Registers[RegisterType::IP]);
-			printf("SP:%#x ",Registers[RegisterType::SP]);
+			printf("RAX:%#x ",Registers[RegisterType::RAX]);
+			printf("RIP:%#x ",Registers[RegisterType::RIP]);
+			printf("RSP:%#x ",Registers[RegisterType::RSP]);
+			printf("\n");
 		}
 		switch (instruction->Type){
 			case DRFRNC:
 				address_as_int stack_top;
-				stack_top.value = *(int64_t*)Registers[RegisterType::SP];
-				*(int64_t*)Registers[RegisterType::SP] = *(stack_top.address);
+				stack_top.value = *(int64_t*)Registers[RegisterType::RSP];
+				*(int64_t*)Registers[RegisterType::RSP] = *(stack_top.address);
 				break;
 			case JMP:
-				Registers[RegisterType::IP] = parameter_value(&(instruction->Parameters[FIRST]));
+				Registers[RegisterType::RIP] = parameter_value(&(instruction->Parameters[FIRST]))-1;
 				break;
 			case NOP:
 				break;
 			case JN:
-				if(Registers[RegisterType::CR])break;
-				Registers[RegisterType::IP] = parameter_value(&(instruction->Parameters[FIRST]));
+				if(Registers[RegisterType::RCR])break;
+				Registers[RegisterType::RIP] = parameter_value(&(instruction->Parameters[FIRST]));
 				break;
 			case PUSH:
-				Registers[RegisterType::SP]-=8;
-				*(int64_t*)Registers[RegisterType::SP] = parameter_value(&(instruction->Parameters[FIRST]));
+				Registers[RegisterType::RSP]-=8;
+				*(int64_t*)Registers[RegisterType::RSP] = parameter_value(&(instruction->Parameters[FIRST]));
 				break;
 			case POP:
-				Registers[instruction->Parameters[FIRST].type] = *(int64_t*)Registers[RegisterType::SP];
-				Registers[RegisterType::SP]+=8;
+				Registers[instruction->Parameters[FIRST].type] = *(int64_t*)Registers[RegisterType::RSP];
+				Registers[RegisterType::RSP]+=8;
 				break;
 			case MOV:
 				if(instruction->ParametersNum == 2){
@@ -94,9 +95,9 @@ void VirtualMachine::Run(InstructionList* instructions,bool log){
 					break;
 				}
 				address_as_int temp;
-				temp.value = *(int64_t*)(Registers[RegisterType::SP]+8);
-				*temp.address = *(int64_t*)Registers[RegisterType::SP];
-				Registers[RegisterType::SP]+=8;
+				temp.value = *(int64_t*)(Registers[RegisterType::RSP]+8);
+				*temp.address = *(int64_t*)Registers[RegisterType::RSP];
+				Registers[RegisterType::RSP]+=8;
 				break;
 			case EXIT:
 				return;
@@ -105,9 +106,18 @@ void VirtualMachine::Run(InstructionList* instructions,bool log){
 				// parameter_value(&(instruction->Parameters[SECOND]));
 				//if(args_count > 6)break;
 				//int64_t args[6];
-				//for(int i = 0; i < args_count; i++,Registers[RegisterType::SP]+=8)
-				//	args[args_count-i-1] = *(int64_t*)Registers[RegisterType::SP];
-				//Registers[RegisterType::AX] = (*Dependencies)[parameter_value(&(instruction->Parameters[FIRST]))]->Run(args,args_count);
+				//for(int i = 0; i < args_count; i++,Registers[RegisterType::RSP]+=8)
+				//	args[args_count-i-1] = *(int64_t*)Registers[RegisterType::RSP];
+				//Registers[RegisterType::RAX] = (*Dependencies)[parameter_value(&(instruction->Parameters[FIRST]))]->Run(args,args_count);
+				break;
+			case RET:
+				Registers[RIP] = *(uint64_t*)Registers[RSP];
+				Registers[RSP] += 8;
+				break;
+			case LEAVE:
+				Registers[RSP] = Registers[RBP];
+				Registers[RBP] = *(uint64_t*)Registers[RSP];
+				Registers[RSP] += 8;
 				break;
 			/* binary operators */
 			default:
@@ -119,12 +129,12 @@ void VirtualMachine::Run(InstructionList* instructions,bool log){
 					);
 					break;
 				}
-				*(int64_t*)(Registers[RegisterType::SP]+8) = binary_operator(
+				*(int64_t*)(Registers[RegisterType::RSP]+8) = binary_operator(
 					instruction->Type,
-					*(int64_t*)(Registers[RegisterType::SP]+8),
-					*(int64_t*)Registers[RegisterType::SP]
+					*(int64_t*)(Registers[RegisterType::RSP]+8),
+					*(int64_t*)Registers[RegisterType::RSP]
 				);
-				Registers[RegisterType::SP]+=8;
+				Registers[RegisterType::RSP]+=8;
 				break;
 		}
 	}
